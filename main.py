c7f5a84e081a9b25dd87b30f90f1797d1620b2c9
abdallah.sarayrah <abdallah.dev85@gmail.com>
2022-10-06 19:46:56 +0300

    buying upgrades

--- main.py
+++ main.py
@@ -11,34 +11,39 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 
 url = "https://orteil.dashnet.org/cookieclicker/"
-driver.set_window_position(700, 0)
+driver.set_window_position(400, 0)
+driver.set_window_size(width=1500, height=1040)
 
 driver.get(url)
 
-time.sleep(2)
+time.sleep(5)
 driver.find_element(By.ID, 'langSelect-EN').click()
-time.sleep(2)
+time.sleep(5)
 
-# products = driver.find_elements(By.CSS_SELECTOR, 'div .locked')
-# print(len(products))
-# for product in products:
-#     product_name = product.find_element(By.CLASS_NAME, 'div.title')
-#     print(product_name.get_attribute("textContent"))
-
-# product = driver.find_element(By.CSS_SELECTOR, '.unlocked .title')
-# print(product.get_property('textContent'))
-
-products = driver.find_element(By.ID, 'products').find_elements(By.CSS_SELECTOR, '.unlocked')
-# print(len(products))
-for product in products:
-    print(product.find_element(By.CLASS_NAME, 'title').get_property('textContent'))
-
+upgrades_count = 0
+products_count = 0
 while True:
-    time.sleep(0.25)
+    time.sleep(0.1)
+
     second = datetime.now().second
+    minute = datetime.now().minute
+
     driver.find_element(By.ID, 'bigCookie').click()
 
-    if second % 30:
-        products = driver.find_element(By.ID, 'products').find_elements(By.CSS_SELECTOR, '.unlocked')
+    if minute % 3 == 0 and second % 60 == 0:
+        # if minute % 5 == 0 and second % 60 == 0:
+        upgrades = driver.find_elements(By.CSS_SELECTOR, '.crate.upgrade.enabled')
+        upgrades_count = len(upgrades)
+        if len(upgrades) > 0:
+            upgrades[-1].click()
+            print('Upgrade bought')
+        else:
+            products = driver.find_elements(By.CSS_SELECTOR, '.product.unlocked.enabled')
+            products_count = len(products)
+            if len(products) > 0:
+                highest_product = products[-1]
+                highest_product.click()
+                print(
+                    f'{highest_product.find_element(By.CSS_SELECTOR, ".title.productName").get_property("textContent")} bought')
 
-
+        print(f"{minute}:{second}:: upgrades: {upgrades_count}, products: {products_count}")
